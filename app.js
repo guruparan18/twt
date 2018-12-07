733ce3e429364ee2d7749bc8d74459fc5da48119
Sree Appachi <guruparan18@users.noreply.github.com>
2018-12-07 15:16:05 -0500

    Update app.js
    Removed keys

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ MongoClient.connect('mongodb://localhost:27017/twitter', function(err, db) {
         var Twit = require('twit');
 
         var T = new Twit({
-            consumer_key: 'H7kUZTLf1xUPk1410ADKb1zQM',
-            consumer_secret: 'AZC8mU6m3Cr8GvwuwCTFaeNLebr17RZBKEYIRDvZmvwP4oNxAi',
-            access_token: '827574158565507072-DDPFfMldxaNwGLPlq55jDLAvfAHPjLb',
-            access_token_secret: 'SLgmtWUll5cOZjUVQmaxDlcVA7i9SWyxZAx0ooprE7Cym',
+            consumer_key: '',
+            consumer_secret: '',
+            access_token: '-',
+            access_token_secret: '',
             timeout_ms: 60 * 1000, // optional HTTP request timeout to apply to all requests.
         });
 
